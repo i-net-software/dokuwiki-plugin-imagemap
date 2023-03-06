7330836fb601b081d4f902c9807b697e3a9caa88
Andreas Gohr <andi@splitbrain.org>
2023-03-06 14:54:57 +0100

    first cleanup
    * switch Test runner form Travis to Github
    * initial quick code/formatting cleanup

--- action.php
+++ action.php
@@ -1,45 +1,38 @@
 <?php
+
 /**
  * DokuWiki Plugin imagemap (Action Component)
  *
  * @license GPL 2 http://www.gnu.org/licenses/gpl-2.0.html
  * @author  Alexander Weggerle <AlexanderWeggerle.de>
  */
+class action_plugin_imagemapping extends DokuWiki_Action_Plugin
+{
 
-// must be run within Dokuwiki
-if (!defined('DOKU_INC')) die();
-
-if (!defined('DOKU_LF')) define('DOKU_LF', "\n");
-if (!defined('DOKU_TAB')) define('DOKU_TAB', "\t");
-if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
-
-require_once DOKU_PLUGIN.'action.php';
-
-class action_plugin_imagemapping extends DokuWiki_Action_Plugin {
-
-    public function register(Doku_Event_Handler $controller) {
+    public function register(Doku_Event_Handler $controller)
+    {
 
         $controller->register_hook('DOKUWIKI_STARTED', 'BEFORE', $this, 'handle_start');
         // $controller->register_hook('TOOLBAR_DEFINE', 'AFTER', $this, 'handle_toolbar_define');
 
     }
 
-    public function handle_start(Doku_Event &$event, $param) {
+    public function handle_start(Doku_Event $event, $param)
+    {
         global $JSINFO;
 
-        $JSINFO['plugin_imagemap_mldummy'] = ml(':wiki:dokuwiki-128.png','', true);
+        $JSINFO['plugin_imagemap_mldummy'] = ml(':wiki:dokuwiki-128.png', '', true);
     }
 
-    public function handle_toolbar_define(Doku_Event &$event, $param) {
+    public function handle_toolbar_define(Doku_Event &$event, $param)
+    {
 
-        $event->data[] = array (
+        $event->data[] = [
             'type' => 'imagemap',
             'title' => 'imagemapping',
             'icon' => '../../plugins/imagemapping/map.png',
-            'list' => array(),
-            );
+            'list' => [],
+        ];
     }
 
 }
-
-// vim:ts=4:sw=4:et:
